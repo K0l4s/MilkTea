6102b2f839c0386116fc546f1da0600c1963a60d
Trung Kien <21110223@student.hcmute.edu.vn>
2023-11-05 16:36:58 +0700

    first commit

---
+++ src/main/java/alotra/milktea/MilkTeaShopsChainManagementApplication.java
@@ -0,0 +1,13 @@
+package alotra.milktea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkTeaShopsChainManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkTeaShopsChainManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alotra/milktea/MilkTeaShopsChainManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package alotra.milktea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkTeaShopsChainManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
