bf11c42e9935865a10dd60ce84b6b235e2200c8b
Trung Kien <21110223@student.hcmute.edu.vn>
2023-12-07 23:54:00 +0700

    Hi

---
+++ src/main/java/alotra/milktea/entity/Cart.java
@@ -0,0 +1,20 @@
+package alotra.milktea.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="customerID")
+    private Customer customer;
+}
---
+++ src/main/java/alotra/milktea/entity/Wallet.java
@@ -0,0 +1,22 @@
+package alotra.milktea.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name ="customerID")
+    private Customer customer;
+
+    private float balance;
+}
