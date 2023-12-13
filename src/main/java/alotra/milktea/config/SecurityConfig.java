7f54d3f40639e8658b22d2964f9e8dd21b640d42
Trung Kien <21110223@student.hcmute.edu.vn>
2023-12-13 15:02:54 +0700

    Security

--- src/main/java/alotra/milktea/config/SecurityConfig.java
+++ src/main/java/alotra/milktea/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package alotra.milktea.config;
 
-import alotra.milktea.entity.User;
 import alotra.milktea.repository.IRoleRepo;
 import alotra.milktea.repository.IUserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,41 +27,58 @@ public class SecurityConfig {
     @Autowired
     IRoleRepo roleRepo;
 
-
     @Bean
     public UserDetailsService userDetailsService() {
-        List<User> users = userRepo.findAll();
+        List<alotra.milktea.entity.User> users = userRepo.findAll();
         List<UserDetails> userDetailsList = new ArrayList<>();
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-        for (User item : users) {
+        for (alotra.milktea.entity.User item : users) {
             UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
                     .username(item.getUsername())
                     .password(passwordEncoder.encode(item.getPassword()))
                     .roles(item.getRole().getName())
+                    .authorities(item.getRole().getName())
+                    .accountLocked(!item.isEnable())
+//                    .disabled(item.isEnable())
                     .build();
             userDetailsList.add(userDetails);
         }
 
-        return new InMemoryUserDetailsManager(userDetailsList);
+        return new InMemoryUserDetailsManager(
+               userDetailsList
+        );
+//        return userDetailsService;
     }
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf-> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
-                        .requestMatchers("/admin/*").hasRole("ADMIN")
-                        .requestMatchers("/delete/*").hasRole("ADMIN")
+                        .requestMatchers("/admin/**").hasAnyAuthority("ADMIN","CREATOR","EDITOR")
+                        .requestMatchers("/add/**").hasAnyAuthority("ADMIN","CREATOR")
+                        .requestMatchers("/edit/**").hasAnyAuthority("ADMIN", "EDITOR")
+                        .requestMatchers("/delete/**").hasAuthority("ADMIN")
+//                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
 
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .failureUrl("/login?error")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/home")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .deleteCookies("username")
+                        .logoutSuccessUrl("/login")
+                        .permitAll());
         return http.build();
     }
 
